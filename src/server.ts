8e7630233edf30bc9d9c682a040f6e5b0505411e
Nutefe <martingbeze52@gmail.com>
2026-03-31 09:03:48 +0000

    Add app & server to start projet

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import { createApp } from "./app";
+import { log } from "node:console";
+
+dotenv.config();
+
+async function main() {
+  const port = Number(process.env.PORT ?? "3000");
+
+  const app = createApp();
+  app.listen(port, () => log("info", "api_listening", { port }));
+}
+
+main().catch((e) => {
+  log("error", "api_crash", { err: String(e) });
+  process.exit(1);
+});
